215846e5313078d3d94568e8d363ea0eeae7230c
childrem <38900005+childrem@users.noreply.github.com>
2018-11-01 11:10:41 -0700

    Update main.js

--- main.js
+++ main.js
@@ -19,6 +19,7 @@ app.set('mysql', mysql);
 app.use('/branch', require('./branch.js'));
 app.use('/department', require('./department.js'));
 app.use('/doctor', require('./doctor.js'));
+app.use('/doctor_branch', require('./doctor_branch.js'));
 app.use('/', express.static('public'));
 
 app.use(function(req,res){
