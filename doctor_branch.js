20e7ad67c47b6713ac576265a30fabd8ebfb18cb
childrem <38900005+childrem@users.noreply.github.com>
2018-11-01 11:40:09 -0700

    Update doctor_branch.js

--- doctor_branch.js
+++ doctor_branch.js
@@ -5,7 +5,7 @@ module.exports = function(){
   // Display all doctor/branch relationships for doctor_branch page
   
     function getDocBranch(res, mysql, context, complete){
-        mysql.pool.query("SELECT b.Name, CONCAT(doc.Fname,' ',doc.Lname) FROM Hosp_Branch b INNER JOIN Hosp_Doctor_Branch db ON b.Id = db.BId INNER JOIN Hosp_Doctor doc ON doc.Id = db.DocId;", function(error, results, fields){
+        mysql.pool.query("SELECT b.Name AS `Branch`, CONCAT(doc.Fname,' ',doc.Lname) AS `Doctor` FROM Hosp_Branch b INNER JOIN Hosp_Doctor_Branch db ON b.Id = db.BId INNER JOIN Hosp_Doctor doc ON doc.Id = db.DocId;", function(error, results, fields){
             if(error){
                 res.write(JSON.stringify(error));
                 res.end();
