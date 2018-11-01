77c18348fe9bf2b8756222ec9773e6c5ab93a2ca
childrem <38900005+childrem@users.noreply.github.com>
2018-10-31 21:01:26 -0700

    Update main.js

--- main.js
+++ main.js
@@ -48,7 +48,7 @@ var handlebars = require('express-handlebars').create({defaultLayout:'main'});
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
-app.set('port', 3000);
+app.set('port', 5728);
 
 app.get('/',function(req,res){
   //var context = {};
