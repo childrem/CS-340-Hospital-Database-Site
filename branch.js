746c096bdf3f68ce771a18467d2d8b5ba32a60ff
childrem <38900005+childrem@users.noreply.github.com>
2018-11-01 15:05:54 -0700

    Update branch.js

--- branch.js
+++ branch.js
@@ -32,5 +32,22 @@ module.exports = function(){
         }
     });
     
+    // Allow user to add new branches via the form and refresh page to view the new row
+    
+    router.post('/', function(req, res){       
+        var mysql = req.app.get('mysql');
+        var sql = "INSERT INTO Hosp_Branch (Name, Street_Address, City, State, Zip_Code, Capacity) VALUES (?,?,?,?,?,?);";
+        var inserts = [req.body.Name, req.body.Street_Address, req.body.City, req.body.State, req.body.Zip_Code, req.body.Capacity];
+        sql = mysql.pool.query(sql,inserts,function(error, results, fields){
+            if(error){
+                console.log(JSON.stringify(error))
+                res.write(JSON.stringify(error));
+                res.end();
+            }else{
+                res.redirect('/branch');
+            }
+        });
+});
+    
     return router;
 }();
