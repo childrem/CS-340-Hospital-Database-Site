f95045bbf51d451d547fbb065854c33d380a6ce8
childrem <38900005+childrem@users.noreply.github.com>
2018-11-01 11:47:57 -0700

    Update doctor_department.js

--- doctor_department.js
+++ doctor_department.js
@@ -1,1 +1,36 @@
+module.exports = function(){
+    var express = require('express');
+    var router = express.Router();
 
+  // Display doctor/department relationships for doctor/department page
+  
+    function getDocDepartment(res, mysql, context, complete){
+        mysql.pool.query("SELECT CONCAT(doc.Fname,' ',doc.Lname) AS `Doctor`, dep.Title AS `Department` FROM Hosp_Doctor doc INNER JOIN Hosp_Doctor_Department dd ON doc.Id = dd.DocId INNER JOIN Hosp_Department dep ON dep.Id = dd.DepartId;", function(error, results, fields){
+            if(error){
+                res.write(JSON.stringify(error));
+                res.end();
+            }
+            context.docdepartments  = results;
+            complete();
+        });
+}
+
+  // When page loads, display all doctor/department relationships
+  
+  router.get('/', function(req, res){
+        var callbackCount = 0;
+        var context = {};
+        //context.jsscripts = ["deleteperson.js","filterpeople.js","searchpeople.js"];
+        var mysql = req.app.get('mysql');
+        getDocDepartment(res, mysql, context, complete);
+        function complete(){
+            callbackCount++;
+            if(callbackCount >= 1){
+                res.render('doctor_department', context);
+            }
+
+        }
+    });
+    
+    return router;
+}();
