6e9680294b5f947e4bdafd56888d4076367b9ce4
childrem <38900005+childrem@users.noreply.github.com>
2018-10-31 20:34:17 -0700

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@
     Uses express, dbcon for database connection, body parser to parse form data 
     handlebars for HTML templates  
 */
-/*
+
 
 var express = require('express');
 var mysql = require('./dbcon.js');
@@ -15,13 +15,13 @@ var handlebars = require('express-handlebars').create({defaultLayout:'main'});
 
 app.engine('handlebars', handlebars.engine);
 app.use(bodyParser.urlencoded({extended:true}));
-app.use('/static', express.static('public'));
+//app.use('/static', express.static('public'));
 app.set('view engine', 'handlebars');
 app.set('port', process.argv[2]);
 app.set('mysql', mysql);
 app.use('/department', require('./department.js'));
 //app.use('/people', require('./people.js'));
-app.use('/', express.static('public'));
+//app.use('/', express.static('public'));
 
 app.use(function(req,res){
   res.status(404);
@@ -38,9 +38,4 @@ app.listen(app.get('port'), function(){
   console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
 });
 
-*/
 
-
-app.get('/', function(req,res){
-    res.render(department);
-});
