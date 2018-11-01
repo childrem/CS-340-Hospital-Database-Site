7a5e8bb0ec8a630713d8a966835bf2eaa6d8f926
childrem <38900005+childrem@users.noreply.github.com>
2018-10-31 17:55:24 -0700

    Update main.js

--- main.js
+++ main.js
@@ -18,8 +18,8 @@ app.use('/static', express.static('public'));
 app.set('view engine', 'handlebars');
 app.set('port', process.argv[2]);
 app.set('mysql', mysql);
-app.use('/people_certs', require('./people_certs.js'));
-app.use('/people', require('./people.js'));
+app.use('/department', require('./department.js'));
+//app.use('/people', require('./people.js'));
 app.use('/', express.static('public'));
 
 app.use(function(req,res){
