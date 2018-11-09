bf0c836aaf0a2a8705563fbdfcda5c2b98386976
childrem <38900005+childrem@users.noreply.github.com>
2018-11-09 15:27:49 -0800

    Update doctor_branch.js

--- doctor_branch.js
+++ doctor_branch.js
@@ -78,5 +78,23 @@ module.exports = function(){
         });
 });
     
+    router.delete('/bid/:bid/docid/:docid', function(req, res){
+        //console.log(req) //I used this to figure out where did pid and cid go in the request
+        console.log(req.params.bid)
+        console.log(req.params.docid)
+        var mysql = req.app.get('mysql');
+        var sql = "DELETE FROM Hosp_Doctor_Branch WHERE BId = ? AND DocId = ?";
+        var inserts = [req.params.bid, req.params.docid];
+        sql = mysql.pool.query(sql, inserts, function(error, results, fields){
+            if(error){
+                res.write(JSON.stringify(error));
+                res.status(400); 
+                res.end(); 
+            }else{
+                res.status(202).end();
+            }
+        })
+})
+    
     return router;
 }();
