fd2a32df95f02f7a8b1831547ccf7000b4b021e0
childrem <38900005+childrem@users.noreply.github.com>
2018-11-08 21:40:58 -0800

    Update deletebranch.js

--- public/deletebranch.js
+++ public/deletebranch.js
@@ -1,1 +1,9 @@
-
+function deleteBranch(Id){
+    $.ajax({
+        url: '/branch/' + Id,
+        type: 'DELETE',
+        success: function(result){
+            window.location.reload(true);
+        }
+    })
+};
