b7e154556ef752122aeb7969430943c1eb5ae62a
childrem <38900005+childrem@users.noreply.github.com>
2018-11-09 16:01:15 -0800

    Update deleteDoctorDepartment.js

--- public/deleteDoctorDepartment.js
+++ public/deleteDoctorDepartment.js
@@ -1,1 +1,14 @@
-
+function deleteDoctorDepartment(DocId, DepartId){
+  $.ajax({
+      url: '/doctor_department/DocId/' + DocId + '/DepartId/' + DepartId,
+      type: 'DELETE',
+      success: function(result){
+          if(result.responseText != undefined){
+            alert(result.responseText)
+          }
+          else {
+            window.location.reload(true)
+          } 
+      }
+  })
+};
