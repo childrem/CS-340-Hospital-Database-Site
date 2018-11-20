da50bec2d348caf58211490372f6e4b39430d2ee
childrem <38900005+childrem@users.noreply.github.com>
2018-11-19 16:26:25 -0800

    Update searchpatients.js

--- public/searchpatients.js
+++ public/searchpatients.js
@@ -2,5 +2,11 @@ function searchPatientsByLastName() {
     //get the last name entered by user in the search bar
     var last_name_to_search  = document.getElementById('last_name_to_search').value
     //construct the URL and redirect to it
-    window.location = '/patient/search/' + encodeURI(last_name_to_search)
+    if (last_name_to_search === ""){
+        window.location = '/patient'
+    }
+    
+    else{
+        window.location = '/patient/search/' + encodeURI(last_name_to_search)
+    }
 }
