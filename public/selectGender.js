882f5ea9f179b9e8786bb0c1481490e61ffab5e8
childrem <38900005+childrem@users.noreply.github.com>
2018-11-21 21:22:51 -0800

    Update selectGender.js

--- public/selectGender.js
+++ public/selectGender.js
@@ -1,4 +1,6 @@
 function selectGender(id){
     //$("#gender-dropdown").val(id);
     document.getElementById("gender-dropdown").value = id;
+    console.log(id);
+    console.log(document.getElementById("gender-dropdown").value);
 }
