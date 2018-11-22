a6744cd7dc47d8ca6d87f579ae927dc77ae35c3c
childrem <38900005+childrem@users.noreply.github.com>
2018-11-21 17:17:00 -0800

    Update updatepatient.js

--- public/updatepatient.js
+++ public/updatepatient.js
@@ -2,7 +2,7 @@ function updatePatient(id){
     $.ajax({
         url: '/patient/' + id,
         type: 'PUT',
-        data: $('#update-patient').serialize(),
+        data: $('#update_patient').serialize(),
         success: function(result){
             window.location.replace("./");
         }
