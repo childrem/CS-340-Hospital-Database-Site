130757dd50d45cf5f74b98095b721b95120d3157
childrem <38900005+childrem@users.noreply.github.com>
2018-11-28 17:26:29 -0800

    Update deleteDoctorDepartment.js

--- public/deleteDoctorDepartment.js
+++ public/deleteDoctorDepartment.js
@@ -1,3 +1,6 @@
+// Code in this file used the code in the following git repository as a foundation:
+// https://github.com/knightsamar/CS340-Sample-Web-App
+
 function deleteDoctorDepartment(DocId, DepartId){
   $.ajax({
       url: '/doctor_department/DocId/' + DocId + '/DepartId/' + DepartId,
